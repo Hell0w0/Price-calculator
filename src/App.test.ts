2fda8216a4e2f86c5605ed9d67ef292f8842e271
CBoden <charlotta.boden@hotmail.com>
2022-04-10 17:51:19 +0200

    e2e test
    End to End test made with Puppeteer

--- src/App.test.ts
+++ src/App.test.ts
@@ -1,5 +1,23 @@
-import App from "./App";
+import * as puppeteer from "puppeteer";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(1+2).toBe(3);
-});
+test("should click around", async () => {
+  var expected = "21";
+  //Setup cromium browser
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 80,
+  });
+
+  const page = await browser.newPage();
+  await page.goto("http://localhost:3000/");
+
+  await page.type("input#price", "1");
+  await page.click("input#userType");
+  await page.focus("input#publishDate");
+  await page.keyboard.type("19000101");
+
+  await page.click("button#calculate");
+
+  const result = await page.$eval("#result", (el) => el.textContent);
+  expect(result).toBe(expected);
+}, 15000);
