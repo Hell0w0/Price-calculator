ebd1ab18bbde4b241e7b9c210e967b0f3e6a8754
CBoden <charlotta.boden@hotmail.com>
2022-04-10 15:56:44 +0200

    Add header to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import "./App.css";
 import { CalculatorApp } from "../refactor/calculator-app/calculator-app";
+import { Header } from "../refactor/header/header";
 function App() {
   return (
     <>
+      <Header />
       <CalculatorApp />
     </>
   );
