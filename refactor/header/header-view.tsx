bf1d3d33c62abf19b81c8a75b0293410207bfff2
CBoden <charlotta.boden@hotmail.com>
2022-04-10 15:55:32 +0200

    Added header

---
+++ refactor/header/header-view.tsx
@@ -0,0 +1,9 @@
+import "./header.css";
+
+export const HeaderView: React.FC = ({}) => {
+  return (
+    <div className="header">
+      <h2>MAG Interactive</h2>
+    </div>
+  );
+};
---
+++ refactor/header/header.tsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+import { HeaderView } from "./header-view";
+
+export const Header: React.FC = () => {
+  return <HeaderView />;
+};
