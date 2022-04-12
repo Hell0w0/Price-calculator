06d3dccec28c06c88df07f565d9b515672d998a0
CBoden <charlotta.boden@hotmail.com>
2022-04-12 16:05:15 +0200

    Fixed select input

--- refactor/tests/App.test.ts
+++ refactor/tests/App.test.ts
@@ -2,18 +2,20 @@ import * as puppeteer from "puppeteer";
 
 // Rerun test script if a new setup is done as a bug with puppeteer occurs.
 test("Runs through a calculation", async () => {
-  var expected = "21";
+  var expected = "31";
   //Setup cromium browser
   const browser = await puppeteer.launch({
     headless: false,
-    slowMo: 80,
   });
 
   const page = await browser.newPage();
   await page.goto("http://localhost:3000/");
-
-  await page.type("input#price", "1");
-  await page.click("input#userType");
+  //Inputs price
+  await page.focus("input#price");
+  await page.keyboard.type("1");
+  //Sets user type
+  await page.select("#userType", "Company user");
+  //Inputs publish date
   await page.focus("input#publishDate");
   await page.keyboard.type("19000101");
 
